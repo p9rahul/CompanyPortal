dacd881367e0c516b14569484122d9c4fde700e5
unknown <p9rahul@gmail.com>
2024-03-03 18:14:31 +0530

    user

--- user/serializers.py
+++ user/serializers.py
@@ -26,3 +26,14 @@ class UserSerializer(serializers.ModelSerializer):
         """ create and return a user with encrypted password."""
         return get_user_model().objects.create_user(**validated_data)
     
+    # def update(self, instance, validated_data):
+    #     """Update a user, setting the password correctly and return it"""
+    #     password = validated_data.pop('password', None)
+    #     user = super().update(instance, validated_data)
+
+    #     if password:
+    #         user.set_password(password)
+    #         user.save()
+
+    #     return user
+    
--- user/views.py
+++ user/views.py
@@ -4,9 +4,7 @@ Doc string : Views for user API
 """
 from rest_framework import generics
 from user.serializers import UserSerializer
-# from django.views.decorators.csrf import csrf_exempt
 
-# @csrf_exempt
 class CreateUserView(generics.CreateAPIView):
     """ Create a new user in the system. """
     serializer_class =UserSerializer
